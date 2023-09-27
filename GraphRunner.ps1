5440cdc33f33029c3f7901f564d4c6a9fbec0ef8
Steve Borosh <rvrsh3ll@users.noreply.github.com>
2023-09-27 15:18:07 -0400

    Update GraphRunner.ps1
    Add Invoke-AutoTokenRefresh

--- GraphRunner.ps1
+++ GraphRunner.ps1
@@ -164,7 +164,121 @@ function Get-GraphTokens{
         }
     }
 }
+function Invoke-AutoTokenRefresh{
+    <#
+        .SYNOPSIS
+        Continuously refresh tokens at an interval.
+        Author: Steve Borosh (@424f424f)
+        License: MIT
+        Required Dependencies: None
+        Optional Dependencies: None
 
+    .DESCRIPTION
+        
+       This module will refresh a Microsoft ID token at specified intervals.
+    
+    .PARAMETER RefreshToken
+        
+        Supply a refresh token to re-authenticate.
+
+    .PARAMETER tenantid
+        
+        Supply a tenant domain or ID to authenticate to.
+
+    .PARAMETER RefreshInterval
+        
+        Supply an interval in minutes to refresh the token. Default 5 minutes.
+    
+    .PARAMETER InitializationDelay
+
+        Supply a delay before starting to refresh in minutes. Devault is 0.
+    
+    .PARAMETER OutFile
+        
+        Supply file name to save to. This will overwrite the current file.
+
+    .EXAMPLE
+        
+        C:\PS> Invoke-AutoTokenRefresh - RefreshToken "0.A.." -tenantid "company.com" -Outfile .\access_token.txt
+        Description
+        -----------
+        This command will use the refresh token to aquire a new access_token, save it to the $tokens variable and to the Outfile.
+    
+
+    #>
+    [cmdletbinding()]
+    Param(
+    [Parameter(Mandatory = $True)]
+    [string]
+    $RefreshToken,
+    [Parameter(Mandatory = $True)]
+    [string]
+    $tenantid,
+    [Parameter(Mandatory = $False)]
+    $RefreshInterval =  5,
+    [Parameter(Mandatory = $False)]
+    $InitializationDelay = 0,
+    [Parameter(Mandatory = $False)]
+    [switch]
+    $DisplayToken,
+    [Parameter(Mandatory = $False)]
+    [string]
+    $Outfile
+    )
+    if($InitializationDelay){
+        Start-Sleep -Seconds (60 * $InitializationDelay)
+    }
+
+    while($true){ 
+        if(!$RefreshToken){
+            write-host -ForegroundColor red '[*] A refresh token is required.'
+            break
+        } elseif (!$tenantid){
+            write-host -ForegroundColor red '[*] The tenant id or domain is required.'
+        }
+        Write-Host -ForegroundColor yellow "[*] Refreshing Tokens..."
+        $authUrl = "https://login.microsoftonline.com/$tenantid"
+        $refreshbody = @{
+                "resource" = "https://graph.microsoft.com/"
+                "client_id" =     "d3590ed6-52b3-4102-aeff-aad2292ab01c"
+                "grant_type" =    "refresh_token"
+                "refresh_token" = $RefreshToken
+                "scope"=         "openid"
+            }
+        $UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+        $Headers=@{}
+        $Headers["User-Agent"] = $UserAgent
+        try {
+            $reftokens = Invoke-RestMethod -UseBasicParsing -Method Post -Uri "$($authUrl)/oauth2/token" -Headers $Headers -Body $refreshbody
+        }
+        catch {
+            $details=$_.ErrorDetails.Message | ConvertFrom-Json
+            Write-Output $details.error
+        } 
+        if($reftokens)
+            {
+                $global:tokens = $reftokens
+                $tokenPayload = $tokens.access_token.Split(".")[1].Replace('-', '+').Replace('_', '/')
+                while ($tokenPayload.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenPayload += "=" }
+                $tokenByteArray = [System.Convert]::FromBase64String($tokenPayload)
+                $tokenArray = [System.Text.Encoding]::ASCII.GetString($tokenByteArray)
+                $tokobj = $tokenArray | ConvertFrom-Json
+                $global:tenantid = $tokobj.tid
+                Write-host "Decoded JWT payload:"
+                $tokobj
+                $global:tokens.access_token | Out-File -encoding ascii -FilePath $Outfile
+                Write-Host -ForegroundColor Green '[*] Successful authentication. Access and refresh tokens have been written to the global $access_token variable. To use them with other GraphRunner modules use the Tokens flag (Example. Invoke-DumpApps -Tokens $tokens)'
+                $baseDate = Get-Date -date "01-01-1970"
+                $tokenExpire = $baseDate.AddSeconds($tokobj.exp).ToLocalTime()
+                Write-Host -ForegroundColor Yellow "[!] Your access token is set to expire on: $tokenExpire"
+                if($DisplayToken){
+                    Write-Host "[*] Your access token is:"
+                    Write-Host $global:tokens.access_token
+                }
+                Start-Sleep -Seconds (60 * $RefreshInterval)
+            }
+        } 
+}
 function Invoke-RefreshGraphTokens{
     <#
         .SYNOPSIS
@@ -4960,6 +5074,7 @@ function List-GraphRunnerModules{
 Invoke-RefreshGraphTokens`t-`t Use a refresh token to obtain new access tokens
 Get-AzureAppTokens`t`t-`t Complete OAuth flow as an app to obtain access tokens
 Invoke-RefreshAzureAppTokens`t-`t Use a refresh token and app credentials to refresh a token
+Invoke-AutoTokenRefresh`t-`t Refresh tokens at an interval.
     "
     Write-Host -ForegroundColor green "----------------- Recon & Enumeration Modules -----------------"
     Write-Host -ForegroundColor green "`tMODULE`t`t`t-`t DESCRIPTION"
